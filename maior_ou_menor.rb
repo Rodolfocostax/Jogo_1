7ad644d992b64b4c0921a0c0918ca9be6091dff5
Rodolfo Costa <rodolfo.costa@locaweb.com.br>
2021-09-16 17:16:07 -0300

    Número de chutes até agora

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -13,34 +13,48 @@ def sorteia_numero_secreto
 	sorteado
 end
 
-def pede_um_numero (tentativa, limite_de_tentativas)
+def pede_um_numero (chutes, tentativa, limite_de_tentativas)
 	puts "\n\n\n\n"	
 	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts  "Chutes até agora" + chutes.to_s
 	puts "Entre com o numero"
 	chute = gets
 	puts "Será que acertou? Você chutou " + chute
 	chute.to_i
 end
+
+def verifica_se_acertou(numero_secreto, chute)
+	acertou = numero_secreto == chute
+
+	if acertou
+		puts "Acertou"
+		return true
+	end
+	maior = numero_secreto > chute
+	if maior 
+		puts "O número secreto é maior!"
+	else
+		puts "O número secreto é menor!"
+	end
+	false
+end
+	
+
+
 limite_de_tentativas = 5
 da_boas_vindas
 numero_secreto = sorteia_numero_secreto
+chutes = []
+total_de_chutes = 0
 
 
 
 
 for tentativa in 1..limite_de_tentativas
-	chute = pede_um_numero(tentativa, limite_de_tentativas)
-	acertou = numero_secreto == chute
-
-	if acertou
-		puts "Acertou"
+	chute = pede_um_numero(chutes, tentativa, limite_de_tentativas)
+	chutes[total_de_chutes] = chute
+	total_de_chutes = total_de_chutes + 1
+	if verifica_se_acertou(numero_secreto, chute)
 		break
-	else
-		maior = numero_secreto > chute
-		if maior 
-			puts "O número secreto é maior!"
-		else
-			puts "O número secreto é menor!"
-		end
 	end
 end
